21bbbff0bdf54d0de8dc2e2cba888792a69bcb2d
OGMalin <oddg@sjakk.biz>
2015-04-22 08:38:40 +0200

    *

--- makelink.cmd
+++ makelink.cmd
@@ -1,3 +1,3 @@
 set JoomlaDir=C:\xampp\htdocs\joomladev
 mklink /J %JoomlaDir%\plugins\content\polarcontent ..\polarcontent
-mklink /J %JoomlaDir%\media\polarcontent polarcontent
+mklink /J %JoomlaDir%\media\polarcontent media\polarcontent
